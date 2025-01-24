4ecadf9f1940f9ca277bf31a69435d18e6a67047
gauravtripathii <gauravtripathii7979@gmail.com>
2025-01-24 20:23:11 +0530

    Hero component > left side

--- src/app/components/home/Hero.jsx
+++ src/app/components/home/Hero.jsx
@@ -3,9 +3,24 @@ import "./Hero.css";
 import Logo from "../Logo";
 
 export default function Hero() {
-    return(
+    return (
         <div className="hero-container">
-            <Logo />
+            <div className="left">
+                <Logo />
+                <div className="description">
+                    <h1>
+                        Predict.
+                        Visualize.
+                        Succeed.
+                    </h1>
+                    <p>
+                        Your partner in business
+                    </p>
+                    <button>Get Started</button>
+                </div>
+            </div>
+
+            <div className="right"></div>
         </div>
     )
 }
