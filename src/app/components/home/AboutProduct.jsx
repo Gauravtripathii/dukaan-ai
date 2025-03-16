21d96f10ecce4d7b168a35563fdcb82839ac0824
gauravtripathii <gauravtripathii7979@gmail.com>
2025-03-16 12:43:48 +0530

    about product desktop

---
+++ src/app/components/home/AboutProduct.jsx
@@ -0,0 +1,20 @@
+import "./AboutProduct.css";
+
+import Image from "next/image";
+
+export default function AboutProduct() {
+    return (
+        <div className="about-product">
+            <h1>
+                Upload sales data, visualize insights, and predict trends with AI-powered tools. Smarter decisions, simplified.
+            </h1>
+            <Image
+                src="/aboutproduct.gif"
+                alt="hero image"
+                width={500}
+                height={500}
+                className="image"
+            />
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import "./page.css";
 
 import Hero from "./components/home/Hero";
+import AboutProduct from "./components/home/AboutProduct";
 
 export default function Home() {
   return (
     <div className="main-container">
       <Hero />
+      <AboutProduct />
     </div>
   );
 }
