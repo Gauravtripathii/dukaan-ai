96e6e7fd61b957f3362f85949fb48828159387b1
gauravtripathii <gauravtripathii7979@gmail.com>
2025-03-20 03:26:02 +0530

    dashboard init

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,15 +1,27 @@
 "use client";
 
+import "@/app/page_styles/dashboard.css";
+
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function DashboardPage() {
+
+    const [forecastedProfits, setForecastedProfits] = useState([]);
+    const [monthlyProjection, setMonthlyProjection] = useState({
+        dates: [],
+        dataArray: [],
+    });
 
     useEffect(() => {
         const getForecast = async () => {
             await axios.get("/api/get-data/")
                 .then(res => {
                     console.log(res.data.forecast);
+                    setMonthlyProjection({
+                        dates: Object.keys(res.data.forecast.forecasted_profits),
+                        dataArray: Object.values(res.data.forecast.forecasted_profits),
+                    });
                 })
                 .catch(error => {
                     console.log(error);
@@ -18,7 +30,11 @@ export default function DashboardPage() {
         getForecast();
     }, [])
 
+    useEffect(() => {
+        console.log(monthlyProjection);
+    }, [monthlyProjection]);
+
     return (
-        <div className="dashboard">dashboard</div>
+        <div className="dashboard-page">dashboard</div>
     );
 }
