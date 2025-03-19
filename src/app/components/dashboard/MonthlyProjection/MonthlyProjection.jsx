7dce5349e9f5a4df9250ec95694dec7de33fd15d
Gauravtripathii <gauravtripathii7979@gmail.com>
2025-03-20 04:17:42 +0530

    loader added for monthly projection component

--- src/app/components/dashboard/MonthlyProjection/MonthlyProjection.jsx
+++ src/app/components/dashboard/MonthlyProjection/MonthlyProjection.jsx
@@ -1,4 +1,5 @@
 import "./MonthlyProjection.css";
+import { PacmanLoader } from "react-spinners";
 
 export default function ForecastedProfits({ monthlyProjection }) {
     return (
@@ -11,7 +12,8 @@ export default function ForecastedProfits({ monthlyProjection }) {
                     <span>(%)</span>
                 </div>
                 <div className="table-body">
-                    {monthlyProjection.dates.map((date, index) => {
+                    {monthlyProjection.dates.length ?
+                    monthlyProjection.dates.map((date, index) => {
                         const value = monthlyProjection.dataArray[index];
                         const bgColor = value < 0 ? '#ffcccc' : value > 0 ? '#ccffcc' : '#f0f0f0';
                         return (
@@ -21,7 +23,16 @@ export default function ForecastedProfits({ monthlyProjection }) {
                                 <span>{(value * 100).toFixed(3)}</span>
                             </div>
                         );
-                    })}
+                    }) :
+                    <div style={{
+                        width: "100%",
+                        padding: "20px",
+                        display: "grid",
+                        placeItems: "center"
+                    }}>
+                        <PacmanLoader />
+                    </div>
+                    }
                 </div>
             </div>
         </div>
